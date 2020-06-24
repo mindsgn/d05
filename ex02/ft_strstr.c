0426083a6641b4e1264f9d7ca41a3271a56614fa
stembe <67050405+stembe@users.noreply.github.com>
2020-06-24 14:01:51 +0200

    Update ft_strstr.c

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
 
+	if (to_find[0] == '\0'){
+		return (str);
+  }
+  
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[j])
+		{
+			j = 1;
+			while (to_find[j] != '\0' && to_find[j] == str[i + j])
+			{
+				if (to_find[j + 1] == '\0')
+					return (&str[i]);
+				j = j + 1;
+			}
+		}
+		i = i + 1;
+	}
+	return (0);
+}
