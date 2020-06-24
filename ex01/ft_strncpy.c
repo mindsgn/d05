71b105c21b3352243edeeb5381d2a729c9292f3d
stembe <67050405+stembe@users.noreply.github.com>
2020-06-24 13:47:58 +0200

    Update ft_strncpy.c

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
 
+	index = 0;
+	while (src[index] != '\0' && index < n)
+	{
+		dest[index] = src[index];
+		index = index + 1;
+	}
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index = index + 1;
+	}
+	return (dest);
+}
